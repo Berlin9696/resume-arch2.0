0a4a2b10e194ad948a7a2bd9651dcddc403c9ab3
Manuel Rosales <rosalesman328@gmail.com>
2025-03-06 10:51:56 -0800

    Updated the UI of the website with a new UI.

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,23 @@
-  //Script for slideshow
-  let slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var slides = document.getElementsByClassName("mySlides");
+    // Hide all slides
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    slideIndex++;
+    // Reset slideIndex if it exceeds number of slides
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-  }
-  ///////////////////////////////////////////
+    // Display the current slide and add a fade effect (if desired)
+    slides[slideIndex - 1].style.display = "block";
+    // Repeat every 3 seconds (3000 milliseconds)
+    setTimeout(showSlides, 3000);
+}
+
   
   function toggleMenu() {
     const menu = document.querySelector(".nav-menu");
